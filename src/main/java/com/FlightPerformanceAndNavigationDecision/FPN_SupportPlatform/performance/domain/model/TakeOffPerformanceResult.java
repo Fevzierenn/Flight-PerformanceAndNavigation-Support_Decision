a58a951e8267ae7ae41af331d292d2f7589acdbe
fevzierenn <fevzierencicek@gmail.com>
2026-04-25 14:04:56 +0300

    feat : add TakeOffPerformanceResult & RunWayLengthRule & TakeOffPerformanceCalculator classes

---
+++ src/main/java/com/FlightPerformanceAndNavigationDecision/FPN_SupportPlatform/performance/domain/model/TakeOffPerformanceResult.java
@@ -0,0 +1,12 @@
+package com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.performance.domain.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TakeOffPerformanceResult {
+    
+    private final double requiredRunwayLength;
+
+}
---
+++ src/main/java/com/FlightPerformanceAndNavigationDecision/FPN_SupportPlatform/performance/domain/rule/RunWayLengthRule.java
@@ -0,0 +1,16 @@
+package com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.performance.domain.rule;
+
+import com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.common.domain.model.Airport;
+
+/**
+ * Domain business rule:
+ * Checks whether the runway length is sufficient for takeoff.
+ */
+
+
+public class RunWayLengthRule {
+    
+    public void validateRunWayLength(double requiredRunwayLength, Airport airport){
+        
+    }
+}
