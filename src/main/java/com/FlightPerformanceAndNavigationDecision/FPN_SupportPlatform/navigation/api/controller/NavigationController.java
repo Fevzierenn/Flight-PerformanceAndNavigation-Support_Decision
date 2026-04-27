4aceeb7f412f34a94841422d056b7c50233fe887
fevzierenn <fevzierencicek@gmail.com>
2026-04-27 15:08:45 +0300

    feat(navigation): update NavigationController to include route distance calculation endpoint

--- src/main/java/com/FlightPerformanceAndNavigationDecision/FPN_SupportPlatform/navigation/api/controller/NavigationController.java
+++ src/main/java/com/FlightPerformanceAndNavigationDecision/FPN_SupportPlatform/navigation/api/controller/NavigationController.java
@@ -1,16 +1,43 @@
 package com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.navigation.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.navigation.api.dtos.CalculateRouteDistanceRequest;
+import com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.navigation.api.dtos.CalculateRouteDistanceResponse;
+import com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.navigation.application.usecase.CalculateRouteDistanceUseCase;
+import com.FlightPerformanceAndNavigationDecision.FPN_SupportPlatform.navigation.domain.model.NavigationResult;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
-@RequestMapping("/navigation")
+@RequestMapping("/api/navigation")
 public class NavigationController {
     
+    private final CalculateRouteDistanceUseCase calculateRouteDistanceUseCase;
+
+    public NavigationController(CalculateRouteDistanceUseCase calculateRouteDistanceUseCase) {
+        this.calculateRouteDistanceUseCase = calculateRouteDistanceUseCase;
+    }
+
 
     @GetMapping("")
     public String navigationControllerHealth() {
         return new String("Navigation Controller works fine");
     }
+
+    @PostMapping("/route-distance")
+    public ResponseEntity<CalculateRouteDistanceResponse> calculateRouteDistance(@Valid @RequestBody CalculateRouteDistanceRequest request) {
+        NavigationResult result = calculateRouteDistanceUseCase.execute(request.getDepartureAirportIcaoCode(), request.getArrivalAirportIcaoCode());
+        return ResponseEntity.ok(new CalculateRouteDistanceResponse(result.getDistanceInKm())) ;
+    }
+    
+
+    
 }
